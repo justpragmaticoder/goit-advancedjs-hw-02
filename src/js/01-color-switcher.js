f06532881bd84f61c2a4887c94a45bbf573e7dff
Vasyl Protsukovych <protcykovich@gmail.com>
2024-05-28 22:43:19 +0200

    Implemented homework

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,28 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+(() => {
+  const startData = document.querySelector("button[data-start]");
+  const stopData = document.querySelector("button[data-stop]");
+  const body = document.querySelector("body");
+
+  let timer = null;
+  dataStop.setAttribute("disabled", true);
+
+  startData.addEventListener("click", () => {
+    startData.removeAttribute("disabled");
+    timer = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    startData.setAttribute("disabled", true);
+  });
+
+  stopData.addEventListener("click", () => {
+    startData.removeAttribute("disabled");
+    clearInterval(timer);
+    stopData.setAttribute("disabled", true);
+  });
+})();
